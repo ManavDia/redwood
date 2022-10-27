93f8ab872d95b388ad917420602ca7bee2e1cf9d
Manav Dhindsa <manav@stafflink.com.au>
2022-10-27 17:07:39 +1000

    Added federated client ids

--- packages/api/src/auth/decoders/cognito.ts
+++ packages/api/src/auth/decoders/cognito.ts
@@ -3,8 +3,22 @@ import jwksClient from 'jwks-rsa'
 
 function verifyCognitoToken(token: string): Promise<null | JwtPayload> {
   return new Promise((resolve, reject) => {
-    const { COGNITO_POOL_ID, COGNITO_REGION, COGNITO_APP_CLIENT_ID } =
-      process.env
+    const {
+      COGNITO_POOL_ID,
+      COGNITO_REGION,
+      COGNITO_APP_CLIENT_ID,
+      COGNITO_FEDERATED_ENV_NAMES,
+    } = process.env
+    const FEDERATED_CLIENT_IDS: string[] = []
+    if (COGNITO_FEDERATED_ENV_NAMES) {
+      const envNames = COGNITO_FEDERATED_ENV_NAMES.split(',')
+      for (const envName of envNames) {
+        const clientId = process.env[envName]
+        if (clientId) {
+          FEDERATED_CLIENT_IDS.push(clientId)
+        }
+      }
+    }
     if (!COGNITO_POOL_ID || !COGNITO_REGION || !COGNITO_APP_CLIENT_ID) {
       throw new Error(
         '`COGNITO_POOL_ID` or `COGNITO_REGION` or `COGNITO_APP_CLIENT_ID` env vars are not set.'
@@ -28,11 +42,16 @@ function verifyCognitoToken(token: string): Promise<null | JwtPayload> {
       },
       (verifyError, decoded) => {
         const payload = decoded as JwtPayload
+        const clientId = payload?.['client_id']
+        const tokenUse = payload?.['token_use']
         if (verifyError) {
           return reject(verifyError)
-        } else if (payload['client_id'] !== COGNITO_APP_CLIENT_ID) {
+        } else if (
+          clientId !== COGNITO_APP_CLIENT_ID &&
+          !FEDERATED_CLIENT_IDS.includes(clientId)
+        ) {
           return reject(`Invalid client_id. Expected: ${COGNITO_APP_CLIENT_ID}`)
-        } else if (payload['token_use'] !== 'access') {
+        } else if (tokenUse !== 'access') {
           return reject(`Invalid token_use. Expected: "access"`)
         }
         resolve(typeof decoded === 'undefined' ? null : payload)
